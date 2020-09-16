ebf2e6c15f64ceda4054a5d56907b7f4b5da5a07
Ender Imen <eimen@boynergrup.com>
2020-09-16 23:27:04 +0300

    Container component created.

---
+++ src/components/Container.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Container'
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
