9face10c83c286c2b2571720d309dcfa62f42948
Ender Imen <eimen@boynergrup.com>
2020-09-16 23:27:28 +0300

    CustomText component created.

---
+++ src/components/CustomText.vue
@@ -0,0 +1,37 @@
+<template>
+  <component :is="tag" :class="size">
+    <slot/>
+  </component>
+</template>
+
+<script>
+export default {
+  name: 'CustomText',
+  props: {
+    tag: {
+      type: String,
+      default: 'b'
+    },
+    size: {
+      type: String,
+      default: 'normal', // normal:16, small:14, xl:32
+      validator: function(value) {
+        return ['normal', 'small', 'xl'].indexOf(value) !== -1
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+.normal {
+  font-size: inherit;
+}
+.small {
+  font-size: 14px;
+}
+.xl{
+  font-size: 32px;
+}
+</style>
