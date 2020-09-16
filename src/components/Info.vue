4f2925d58f5f29fc0b37a77d647a9841ed895b79
Ender Imen <eimen@boynergrup.com>
2020-09-16 23:56:30 +0300

    Full date has been calculated and displayed.

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,11 +1,7 @@
 <template>
   <section class="m-info">
-    <CustomText :tag="'h3'" class="m-info__location"
-      >İstanbul, Türkiye</CustomText
-    >
-    <CustomText :tag="'p'" class="m-info__date"
-      >16 Eylül 2020 Çarşamba</CustomText
-    >
+    <CustomText :tag="'h3'" class="m-info__location">İstanbul, Türkiye</CustomText>
+    <CustomText :tag="'p'" class="m-info__date">{{ getFullYear() }}</CustomText>
   </section>
 </template>
 
@@ -13,7 +9,54 @@
 import CustomText from "@/components/CustomText";
 export default {
   name: "Info",
-  components: { CustomText }
+  components: { CustomText },
+  data() {
+    return {
+      days: [
+        "Pazartesi",
+        "Salı",
+        "Çarşamba",
+        "Perşembe",
+        "Cuma",
+        "Cumartesi",
+        "Pazar"
+      ],
+      months: [
+        "Ocak",
+        "Şubat",
+        "Mart",
+        "Nisan",
+        "Mayıs",
+        "Haziran",
+        "Temmuz",
+        "Ağustos",
+        "Eylül",
+        "Ekim",
+        "Kasım",
+        "Aralık"
+      ],
+      date: new Date()
+    };
+  },
+  computed: {
+    getDay() {
+      return this.days[this.date.getDay() - 1];
+    },
+    getMonth() {
+      return this.months[this.date.getMonth()];
+    },
+    getYear() {
+      return this.date.getFullYear();
+    },
+    getDate() {
+      return this.date.getDate();
+    }
+  },
+  methods: {
+    getFullYear() {
+      return `${this.getDate} ${this.getMonth} ${this.getYear} ${this.getDay}`;
+    }
+  }
 };
 </script>
 
