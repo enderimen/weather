371151785d3362e28e90e0f129c7a3ad713a1633
Ender Imen <eimen@boynergrup.com>
2020-09-17 23:45:31 +0300

    Weather information was taken from api.

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,92 @@
 <template>
   <Container>
-    <section class="wrapper">
+    <header class="wrapper">
       <input
         class="a-location__input"
         type="text"
         placeholder="İl veya ilçe giriniz"
         v-model="location"
+        @keypress.enter="fetchWeather"
       />
-      <Info />
-      <div class="a-degree">
-        19° {{location}}
-      </div>
-
+      <Info :city="city" />
+      <div class="a-degree">{{ weatherList.length > 0 ? weatherList[0].degree : "" }}°</div>
       <div class="a-weather__status">
-        <CustomText size="xl">Güneşli</CustomText>
+        <CustomText :size="'xl'">{{ weatherList.length > 0 ? weatherList[0].description : "" }}</CustomText>
       </div>
-    </section>
+    </header>
+
+    <footer class="o-status__list">
+      <OtherDay/>
+      <OtherDay/>
+      <OtherDay/>
+    </footer>
   </Container>
 </template>
 
 <script>
 import Container from "@/components/Container";
 import Info from "@/components/Info";
+import CustomText from "@/components/CustomText";
+import OtherDay from "@/components/OtherDay";
+
 export default {
   name: "App",
   components: {
+    OtherDay,
     Info,
     Container,
+    CustomText
   },
   data() {
     return {
-      location: ""
+      apiKey: "apikey 4NVazvZ7iHVZPecCO9106Y:0K052LPX0mgKnWiC0Y5UY4",
+      apiUrl:
+        "https://api.collectapi.com/weather/getWeather?data.lang=tr&data.city=",
+      location: "İstanbul",
+      weatherList: [],
+      city: ""
     };
+  },
+  created() {
+    this.fetchWeather();
+  },
+  methods: {
+    fetchWeather() {
+      fetch(this.apiUrl + this.location, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: this.apiKey
+        }
+      })
+        .then(response => {
+          response.json().then(data => {
+            this.city = data.city;
+            this.clearAllWeatherField(data.result);
+          });
+        })
+        .catch(() => {
+          console.log("Bağlantı kurulurken sorun oluştu!");
+        });
+    },
+    clearAllWeatherField(weatherList) {
+      this.weatherList = [];
+
+      weatherList.map(weather => {
+        this.weatherList.push({
+          degree: parseInt(weather.degree),
+          humidity: parseInt(weather.humidity),
+          description: weather.description,
+          max: parseInt(weather.max),
+          min: parseInt(weather.min),
+          night: parseInt(weather.night),
+          status: weather.status,
+          image: weather.icon
+        });
+      });
+
+      console.log(this.weatherList);
+    }
   }
 };
 </script>
@@ -41,10 +96,7 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: flex-start;
-  background-image: url("./assets/chill.png");
-  background-size: cover;
-  height: 100vh;
+  justify-content: space-around;
   color: var(--txt-color-light);
 }
 
@@ -71,6 +123,15 @@ export default {
   font-size: 6rem;
   font-weight: 300;
   text-shadow: 3px 8px 9px var(--txt-shadow-color);
-  margin-top: calc(100% - 75%);
+  margin-top: calc(100% - 95%);
+}
+
+.o-status__list {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  background-color: red;
 }
 </style>
